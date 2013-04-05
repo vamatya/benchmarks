38d287119dd3b419f24765f3c1a15c153ad8dafc
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-04-05 17:20:15 -0500

    Avoiding copy on sending site

--- benchmarks/network/osu_bw.cpp
+++ benchmarks/network/osu_bw.cpp
@@ -12,9 +12,7 @@
 #include <boost/shared_ptr.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
-void isend(hpx::util::serialize_buffer<char> const& )
-{
-}
+void isend(hpx::util::serialize_buffer<char> const& receive_buffer) {}
 HPX_PLAIN_ACTION(isend);
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,16 +39,21 @@ unsigned long getpagesize()
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
+#define LOOP_SMALL  100
+#define WINDOW_SIZE_SMALL  64
+#define SKIP_SMALL  10
+
 #define LOOP_LARGE  20
 #define WINDOW_SIZE_LARGE  64
 #define SKIP_LARGE  2
+
 #define LARGE_MESSAGE_SIZE  8192
+
 #define MAX_MSG_SIZE (1<<22)
 #define MAX_ALIGNMENT 65536
-#define MYBUFSIZE (MAX_MSG_SIZE + MAX_ALIGNMENT)
+#define SEND_BUFSIZE (MAX_MSG_SIZE + MAX_ALIGNMENT)
 
-char send_buffer[MYBUFSIZE];
-char receive_buffer[MYBUFSIZE];
+char send_buffer[SEND_BUFSIZE];
 
 void run_benchmark()
 {
@@ -60,21 +63,22 @@ void run_benchmark()
     if (!localities.empty())
         dest = localities[0];
 
+    // align used buffers on page boundaries
     unsigned long align_size = getpagesize();
     int loop = 100;
     int window_size = 64;
     int skip = 10;
 
-    char* aligned_send_buffer = align_buffer(send_buffer, align_size);
-    char* aligned_receive_buffer = align_buffer(receive_buffer, align_size);
-
     BOOST_ASSERT(align_size <= MAX_ALIGNMENT);
-    for(std::size_t size = 1; size <= MAX_MSG_SIZE; size *= 2)
+
+    char* aligned_send_buffer = align_buffer(send_buffer, align_size);
+
+    // perform actual measurements
+    for (std::size_t size = 1; size <= MAX_MSG_SIZE; size *= 2)
     {
         std::memset(aligned_send_buffer, 'a', size);
-        std::memset(aligned_receive_buffer, 'b', size);
 
-        if(size > LARGE_MESSAGE_SIZE) {
+        if (size > LARGE_MESSAGE_SIZE) {
             loop = LOOP_LARGE;
             skip = SKIP_LARGE;
             window_size = WINDOW_SIZE_LARGE;
@@ -82,6 +86,7 @@ void run_benchmark()
 
         hpx::util::high_resolution_timer t;
         for (int i = 0; i != loop + skip; ++i) {
+            // do not measure warm up phase
             if (i == skip)
                 t.restart();
 
@@ -91,10 +96,13 @@ void run_benchmark()
             isend_action send;
             for (int j = 0; j < window_size; ++j)
             {
-                lazy_results.push_back(hpx::async(send, dest,
-                    hpx::util::serialize_buffer<char>(aligned_send_buffer, size)));
-            }
+                typedef hpx::util::serialize_buffer<char> buffer_type;
 
+                // Note: The original benchmark uses MPI_Isend which does not
+                //       create a copy of the passed buffer.
+                lazy_results.push_back(hpx::async(send, dest,
+                    buffer_type(aligned_send_buffer, size, buffer_type::reference)));
+            }
             hpx::wait_all(lazy_results);
         }
 
